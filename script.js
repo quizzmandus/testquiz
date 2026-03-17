78b9db5c1ac56f1db9f601583bf824ad2b5ceca8
quizzmandus <bidon59@gmail.com>
2026-03-17 16:53:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -86,10 +86,11 @@ nextBtn.onclick = () => {
 
   loadQuestion();
 };
+
+loadQuestion();
 progressBar.value = 0;
 currentTimeEl.textContent = "0:00";
 durationEl.textContent = "0:00";
-loadQuestion();
   audio.addEventListener("timeupdate", () => {
   const progress = (audio.currentTime / audio.duration) * 100;
   progressBar.value = progress || 0;
